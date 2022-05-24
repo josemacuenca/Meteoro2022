45c16474d1c1db26eb043134bcd81ea6abb3711b
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-05-24 11:11:13 -0300

    Add/ getResourcePerRole in apex service

--- force-app/main/default/lwc/assignResource/assignResource.js
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -1,12 +1,15 @@
 import { api, LightningElement, wire } from "lwc";
 import getProjectLineItems from "@salesforce/apex/ProjectDataService.getProjectLineItems";
+import getResourcePerRole from "@salesforce/apex/ProjectDataService.getResourcePerRole";
 
 export default class AssignResource extends LightningElement {
   ProjectLineItems;
   projectId = "";
+  Resource;
+  role = "";
 
     @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
-    wiredBoats({ data, error }) {
+    wiredProjectLineItems({ data, error }) {
       if (data) {
         this.ProjectLineItems = data;
         console.log("  this.ProjectLineItems",  data)
@@ -15,4 +18,17 @@ export default class AssignResource extends LightningElement {
         console.log("data.error");
         console.log(error);
       }
-    }}
+    }
+  
+    @wire(getResourcePerRole, { role: "Consultant" } )
+    wiredResources({ data, error }) {
+      if (data) {
+        this.Resource = data;
+        console.log("  this.Resource",  data)
+        console.log("this.Resource",  this.Resource)
+      } else if (error) {
+        console.log("data.error");
+        console.log(error);
+      }
+    }
+  }
