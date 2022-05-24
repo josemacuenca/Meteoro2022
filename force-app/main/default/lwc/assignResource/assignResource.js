5fe6096283b0182cd6e9f55ab065692a436b567c
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-05-24 02:57:51 -0300

    Add/ Lwc AssignResource

---
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -0,0 +1,6 @@
+import { api, LightningElement, wire } from "lwc";
+import getBoats from "@salesforce/apex/BoatDataService.getBoats";
+
+export default class AssignResource extends LightningElement {
+
+}
