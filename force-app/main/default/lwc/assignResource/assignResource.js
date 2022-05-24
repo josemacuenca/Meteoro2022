5c553cf93d1b96cd7ccf13c7acbb4a0ed3fcc0ac
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-05-24 04:22:32 -0300

    Add/ getProjecLineItems apexService

--- force-app/main/default/lwc/assignResource/assignResource.js
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -1,6 +1,18 @@
 import { api, LightningElement, wire } from "lwc";
-import getBoats from "@salesforce/apex/BoatDataService.getBoats";
+import getProjectLineItems from "@salesforce/apex/ProjectDataService.getProjectLineItems";
 
 export default class AssignResource extends LightningElement {
+  ProjectLineItems;
+  projectId = "";
 
-}
+    @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
+    wiredBoats({ data, error }) {
+      if (data) {
+        this.ProjectLineItems = data;
+        console.log("  this.ProjectLineItems",  data)
+        console.log("this.ProjectLineItems",  this.ProjectLineItems)
+      } else if (error) {
+        console.log("data.error");
+        console.log(error);
+      }
+    }}
