3784a14283a6b1c2f1122dbbb37c60fb696cf7f1
josemacuenca <josemacuenca@gmail.com>
2022-05-26 10:55:06 -0300

    cc100 flow create project v3

---
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -0,0 +1,34 @@
+import { api, LightningElement, wire } from "lwc";
+import getProjectLineItems from "@salesforce/apex/ProjectDataService.getProjectLineItems";
+import getResourcePerRole from "@salesforce/apex/ProjectDataService.getResourcePerRole";
+
+export default class AssignResource extends LightningElement {
+  ProjectLineItems;
+  projectId = "";
+  Resource;
+  role = "";
+
+    @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
+    wiredProjectLineItems({ data, error }) {
+      if (data) {
+        this.ProjectLineItems = data;
+        console.log("  this.ProjectLineItems",  data)
+        console.log("this.ProjectLineItems",  this.ProjectLineItems)
+      } else if (error) {
+        console.log("data.error");
+        console.log(error);
+      }
+    }
+  
+    @wire(getResourcePerRole, { role: "Consultant" } )
+    wiredResources({ data, error }) {
+      if (data) {
+        this.Resource = data;
+        console.log("  this.Resource",  data)
+        console.log("this.Resource",  this.Resource)
+      } else if (error) {
+        console.log("data.error");
+        console.log(error);
+      }
+    }
+  }
