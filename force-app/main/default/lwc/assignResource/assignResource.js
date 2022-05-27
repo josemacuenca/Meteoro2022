5731a8d193a53d76b352e39343844588451a3523
josemacuenca <josemacuenca@gmail.com>
2022-05-27 08:06:48 -0300

    CC666 para demo

--- force-app/main/default/lwc/assignResource/assignResource.js
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, api, wire } from "lwc";
 import getProjectLineItems from "@salesforce/apex/ProjectDataService.getProjectLineItems";
  
 export default class AssignResource extends LightningElement {
@@ -8,7 +8,8 @@ export default class AssignResource extends LightningElement {
 
   // @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
 // Aqui parra hacer dinammico tengo que crrear un nuevo componente que me traiga el id mediante evento click
-  @wire(getProjectLineItems)
+  @api recordId
+  @wire(getProjectLineItems , { projectId: '$recordId' } )
   wiredProjectLineItems({ data, error }) {
     if (data) {
       this.projectLineItems = data;
