94d4fa15a5097109df948f89b9e9165c16c820b3
Victor Bosio <bosiovictor@gmail.com>
2022-05-31 20:19:03 -0300

    Initial assign resources implementation

--- force-app/main/default/lwc/assignChild/assignChild.js
+++ force-app/main/default/lwc/assignChild/assignChild.js
@@ -3,10 +3,11 @@ import { LightningElement, api, wire } from "lwc";
 import getAllResourcePerRole from "@salesforce/apex/ProjectDataService.getAllResourcePerRole";
 
 const columns = [
-  { label: "Resourse", fieldName: "Name" },
-  { label: "Precio por hora", fieldName: "RatePerHour__c", type: "currency" },
-  { label: "Star Date", fieldName: "starDate", type: "date", editable: true },
-  { label: "End Date", fieldName: "endDate", type: "date", editable: true },
+  { label: "Resource", fieldName: "Name" },
+  { label: "Rate per Hour", fieldName: "RatePerHour__c", type: "currency" },
+  { label: "Start Date", fieldName: "Start_Date__c", type: "date-local", editable: true },
+  { label: "End Date", fieldName: "End_Date__c", type: "date-local", editable: true },
+  { label: "Squad Leader", fieldName: "Squad_Leader__c", type: "boolean", editable: true },
 ];
 export default class AssignChild extends LightningElement {
   @api
@@ -14,17 +15,34 @@ export default class AssignChild extends LightningElement {
   resourcefiltered;
   data;
   columns = columns;
-  // @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
+  ResourcesJson = [];
+  // // @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
 
+
+
+  handleSave(event) {
+    this.saveDraftValues = event.detail.draftValues;
+    this.ResourcesJson.push(this.saveDraftValues);
+   this.saveDraftValues.forEach(element => this.ResourcesJson.push(element));
+  //  this.ResourcesJson.shift();
+   console.log('saveDraftValues',this.ResourcesJson);
+    // console.log(this.saveDraftValues);
+    // console.log("0[0]",this.saveDraftValues[1]);
+  }
+  
   @wire(getAllResourcePerRole, { resourceRole: "$plitem.Role__c" })
   wiredAllResources({ data, error }) {
     if (data) {
       this.resourcefiltered = data;
       this.data = data;
-      console.log("this.User", this.resourcefiltered);
+      // console.log("this.User", this.resourcefiltered);
     } else if (error) {
       console.log("data.error");
       console.log(error);
     }
   }
+
+
+
+
 }
