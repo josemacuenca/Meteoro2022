cd87f2ac56a8bc76051e2ecdc64513d95b5cba96
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-06-01 15:48:30 -0300

    CC305 Asign resources componet lwc and wireds

--- force-app/main/default/lwc/assignResource/assignResource.js
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -2,46 +2,31 @@ import { LightningElement, api, wire } from "lwc";
 import getProjectLineItems from "@salesforce/apex/ProjectDataService.getProjectLineItems";
  
 export default class AssignResource extends LightningElement {
-  projectLineItems;
- 
-  // projectId = "";
-
-  // @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
-// Aqui parra hacer dinammico tengo que crrear un nuevo componente que me traiga el id mediante evento click
+  // selectedProjectLineItem;
+  projectLineItemsOptions; 
   @api recordId
   @wire(getProjectLineItems , { projectId: '$recordId' } )
   wiredProjectLineItems({ data, error }) {
     if (data) {
-      this.projectLineItems = data;
-      console.log("  this.ProjectLineItems", data);
-      console.log("this.ProjectLineItemsssssss", this.projectLineItems);
+      // data.map(item => this.projectLineItemsOptions.push(item.Role__c + " " + item.Quantity_hours__c))
+      
+      this.projectLineItemsOptions = data.map((item) => {
+        return { label: item.Role__c + " " + item.Quantity_hours__c, value: item.Role__c};
+      });
+      console.log("this.ProjectLineItemsssssss", this.projectLineItemsOptions);
     } else if (error) {
       console.log("data.error");
       console.log(error);
     }
-  }
-
-  // @wire(getAllResourcePerRole)
-  // wiredAllResources({ data, error }) {
-  //   if (data) {
-  //     this.user = data;
-  //     console.log("  this.User", data);
-  //     console.log("this.User", this.user);
-  //   } else if (error) {
-  //     console.log("data.error");
-  //     console.log(error);
-  //   }
-  // }
-
-  // @wire(getResourcePerRole)
-  // wiredResources({ data, error }) {
-  //   if (data) {
-  //     this.Resource = data;
-  //     console.log("  this.Resource", data);
-  //     console.log("this.Resource", this.Resource);
-  //   } else if (error) {
-  //     console.log("data.error");
-  //     console.log(error);
-  //   }
-  // }
+  } 
+ 
+ 
+  handleProjectLineItemsChange(event) {
+    // console.log("eventquetrraeeeee",event)
+    // Create the const searchEvent
+    // searchEvent must be the new custom event search
+    this.selectedProjectLineItem = event.detail.value;
+console.log(this.selectedProjectLineItem)   
+   }
+  
 }
