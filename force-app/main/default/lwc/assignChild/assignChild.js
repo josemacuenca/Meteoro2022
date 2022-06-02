5333b8adb5ace280280cc7076ce10fbdecc1134c
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-06-02 15:06:06 -0300

    New fetch data to Assign Child

--- force-app/main/default/lwc/assignChild/assignChild.js
+++ force-app/main/default/lwc/assignChild/assignChild.js
@@ -2,47 +2,14 @@
 import { LightningElement, api, wire } from "lwc";
 import getAllResourcePerRole from "@salesforce/apex/ProjectDataService.getAllResourcePerRole";
 
-const columns = [
-  { label: "Resource", fieldName: "Name" },
-  { label: "Rate per Hour", fieldName: "RatePerHour__c", type: "currency" },
-  { label: "Start Date", fieldName: "Start_Date__c", type: "date-local", editable: true },
-  { label: "End Date", fieldName: "End_Date__c", type: "date-local", editable: true },
-  { label: "Squad Leader", fieldName: "Squad_Leader__c", type: "boolean", editable: true },
-];
+ 
 export default class AssignChild extends LightningElement {
+  
   @api
   plitem;
-  resourcefiltered;
-  data;
-  columns = columns;
-  ResourcesJson = [];
-  // // @wire(getProjectLineItems, { projectId: "a058a00000IGYN6AAP" } )
-
-
-
-  handleSave(event) {
-    this.saveDraftValues = event.detail.draftValues;
-    this.ResourcesJson.push(this.saveDraftValues);
-   this.saveDraftValues.forEach(element => this.ResourcesJson.push(element));
-  //  this.ResourcesJson.shift();
-   console.log('saveDraftValues',this.ResourcesJson);
-    // console.log(this.saveDraftValues);
-    // console.log("0[0]",this.saveDraftValues[1]);
+ 
+   
+  handleClick(){
+    console.log("this.plitem",this.plitem.)
   }
-  
-  @wire(getAllResourcePerRole, { resourceRole: "$plitem.Role__c" })
-  wiredAllResources({ data, error }) {
-    if (data) {
-      this.resourcefiltered = data;
-      this.data = data;
-      // console.log("this.User", this.resourcefiltered);
-    } else if (error) {
-      console.log("data.error");
-      console.log(error);
-    }
-  }
-
-
-
-
 }
--- force-app/main/default/lwc/assignResource/assignResource.js
+++ force-app/main/default/lwc/assignResource/assignResource.js
@@ -1,5 +1,6 @@
 import { LightningElement, api, wire } from "lwc";
 import setProjectLineResource from "@salesforce/apex/ProjectDataService.setProjectLineResource";
+import getAllResourcePerRole from "@salesforce/apex/ProjectDataService.getAllResourcePerRole";
  
 export default class AssignResource extends LightningElement {
   allProjectLineItems;
@@ -12,7 +13,8 @@ export default class AssignResource extends LightningElement {
     if (data) {
       this.allProjectLineItems = data
       // data.map(item => this.projectLineItemsOptions.push(item.Role__c + " " + item.Quantity_hours__c))
-      
+      console.log("this.ProjectLineItemsssssss", this.allProjectLineItems);
+
       this.projectLineItemsOptions = data.map((item) => {
         return { label: item.Role__c + " " + item.Quantity_hours__c, value: item.Role__c};
       });
@@ -31,5 +33,43 @@ export default class AssignResource extends LightningElement {
     this.selectedProjectLineItem = event.detail.value;
 console.log("selectedProjectLineItem",this.selectedProjectLineItem)   
    }
-  
+        //  ------------------------------------------------------------------.--------------------------
+
+    
+    selectedResource;
+    resourcefilteredOptions;
+    @wire(getAllResourcePerRole, { resourceRole: '$selectedProjectLineItem' })
+    wiredAllResources({ data, error }) {
+      if (data) {
+        
+        this.resourcefilteredOptions = data.map((item) => {
+          return { label: item.Name + " | Precio x Hour $" + item.RatePerHour__c , value: item.Id};
+        });
+        console.log("this.Users filtrados", this.resourcefilteredOptions  );
+      } else if (error) {
+        console.log("data.error");
+        console.log(error);
+      }
+    }
+    
+    handleResourcePerRoleChange(event) {
+      this.selectedResource = event.detail.value;
+      console.log("id user p la query",this.selectedResource)   
+     }
+        //  ------------------------------------------------------------------.--------------------------
+
+
+     arrayAssignSelected=[];
+
+     handleAssignSubmit(){      
+       this.arrayAssignSelected.push(this.selectedResource)
+       this.arrayAssignSelected.push(this.selectedProjectLineItem)
+       console.log("arrayAssignSelected",this.arrayAssignSelected) 
+ 
+       // this.arrayAssignSelected.push({'Resource':this.selectedResource})
+       // this.arrayAssignSelected.push({'ProjectLineItem':this.selectedProjectLineItem})
+     }
+
+ //  ------------------------------------------------------------------.--------------------------
+
 }
