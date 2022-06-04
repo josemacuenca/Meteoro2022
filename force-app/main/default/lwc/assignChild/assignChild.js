533550a8d5c19d4793ab13140e098897e85d6811
fedebati <federicobatistella@gmail.com>
2022-06-04 19:24:47 -0300

    agregado del boton edit y la funcionalidad

--- force-app/main/default/lwc/assignChild/assignChild.js
+++ force-app/main/default/lwc/assignChild/assignChild.js
@@ -1,10 +1,27 @@
 // import { LightningElement, api, wire } from "lwc";
 import { LightningElement, api } from "lwc";
 import getAllResourcePerRole from "@salesforce/apex/ProjectDataService.getAllResourcePerRole";
-
+import { NavigationMixin } from "lightning/navigation";
  
-export default class AssignChild extends LightningElement {
+export default class AssignChild extends NavigationMixin(LightningElement) {
   
-   @api plitem;
+   @api 
+   plitem;
+   
+   editRecord;
  
+   
+   handleEditRecord(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.editRecord = event.target.dataset.id;
+      this[NavigationMixin.Navigate]({
+        type: "standard__recordPage",
+        attributes: {
+          recordId: this.editRecord,
+          objectApiName: "ProjectLine_Resource__c",
+          actionName: "edit",
+        },
+      });
+    }
 }
