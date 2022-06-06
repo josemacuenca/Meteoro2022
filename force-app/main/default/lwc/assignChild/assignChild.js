d97482cebcedf3ed41e15e6880dd3a33322425b9
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-06-06 10:01:58 -0300

    fixing delete button

--- force-app/main/default/lwc/assignChild/assignChild.js
+++ force-app/main/default/lwc/assignChild/assignChild.js
@@ -34,51 +34,13 @@ export default class AssignChild extends NavigationMixin(LightningElement) {
       
     }
     handleDelete(event) {
-      // event.preventDefault();
+      event.preventDefault();
       // event.stopPropagation();
-      // this.deleteRecord = event.target.dataset.id;
-      // var recordId = JSON.parse(JSON.stringify((this.deleteRecord)))
-      //  var recordId = JSON.stringify(this.deleteRecord) 
       
-          //  const recordId = this.deleteRecord
            const recordId =JSON.parse(JSON.stringify(event.target.dataset.id))
          //   this.mapAssigned = JSON.parse(JSON.stringify(value));
       console.log("deleteRecord",JSON.stringify(recordId));
-      deleteRecord(recordId)
-    
-    
-      // .then(() =>{
-
-
-
-      //   const toastEvent = new ShowToastEvent({
-        
-      //   title:'Record Deleted',
-        
-      //   message:'Record deleted successfully',
-        
-      //   variant:'success',
-        
-      //   })
-        
-      //   this.dispatchEvent(toastEvent);
-        
-        
-        
-      //   return refreshApex(this.getContact);
-        
-      //   })
-        
-      //   .catch(error =>{
-        
-      //   window.console.log('Unable to delete record due to ' + error.body.message);
-        
-      //   });
-        
-        
-        
-    
-      
+      deleteRecord(recordId)           
       .then(() => {
         this.dispatchEvent(
             new ShowToastEvent({
@@ -110,42 +72,4 @@ export default class AssignChild extends NavigationMixin(LightningElement) {
     }
     
     
-    // handleDelete(event) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    //   this.deleteRecord = event.target.dataset.id;
-    //   var recordId = JSON.parse(JSON.stringify((this.deleteRecord)))
-    //      //   this.mapAssigned = JSON.parse(JSON.stringify(value));
-
-    //   console.log("deleteRecord",recordId);
-    //   deleteRecord(recordId)
-    //   .then(() => {
-    //     this.dispatchEvent(
-    //         new ShowToastEvent({
-    //             title: 'Success',
-    //             message: 'Record deleted',
-    //             variant: 'success'
-    //         }),
-    //     window.location.reload()
-    //     );
-
-    //     // this[NavigationMixin.Navigate]({
-    //     //     type: 'standard__objectPage',
-    //     //     attributes: {
-    //     //         objectApiName: 'Contact',
-    //     //         actionName: 'home',
-    //     //     },
-    //     // });
-    // })
-    // .catch(error => {
-    //     this.dispatchEvent(
-    //         new ShowToastEvent({
-    //             title: 'Error deleting record',
-    //             message: error.body.message,
-    //             variant: 'error'
-    //         })
-    //     );
-    // });
-       
-    // }
   }
