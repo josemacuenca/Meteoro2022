6014474acbaf80edea8bb15ad41e2ffb874b80d1
patriciodfernandez <patricio.dfernandez@gmail.com>
2022-06-07 16:38:56 -0300

    a

--- force-app/main/default/lwc/loadOfHours/loadOfHours.js
+++ force-app/main/default/lwc/loadOfHours/loadOfHours.js
@@ -7,12 +7,18 @@ import getProjectTaskPorResource from '@salesforce/apex/ProjectDataService.getPr
 
 export default class LoadOfHours extends LightningElement {
 
+  TaskQuantityInProject;
   AllProjectLineResources;
   @wire(getProjectTaskPorResource)
   handleRequestPrjTaskPorRec({data, error}) {
     if (data) {
       this.AllProjectLineResources = data;
-           console.log("this.AllProjectLineResources", this.AllProjectLineResources);
+           console.log("this.AllProjectLineResources" , this.AllProjectLineResources);
+       
+      this.TaskQuantityInProject = this.AllProjectLineResources[0].Project_Tasks__r.length 
+      
+      console.log("this.TaskQuantityInProject" , this.TaskQuantityInProject);
+
              } 
              else if (error) {
       console.log("data.error");
